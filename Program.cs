ce9425f80fbf17c80b45adc7305377c4a4849c7f
Drakkura <gonhgyhjj1@gmail.com>
2023-03-27 03:05:54 +0700

    implementasi design  by contract

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tpmodul6_1302210124;
+
+SayaTubeVideo sayaTubeVideo = new SayaTubeVideo("Tutorial Design By Contract - Muhammad Ryan Piskadinata");
+sayaTubeVideo.IncreasePlayCount(124);
+sayaTubeVideo.PrintVideoDetail();
